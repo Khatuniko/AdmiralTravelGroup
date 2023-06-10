15a06c9d1d96f69e9ccf12b83b27c3700d6d9986
Khatuna Chaphodze <111879031+Khatuniko@users.noreply.github.com>
2023-06-11 03:11:45 +0400

    start

--- src/app/module/main-pg/header/fast-contact/fast-contact.component.ts
+++ src/app/module/main-pg/header/fast-contact/fast-contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fast-contact',
@@ -8,11 +9,19 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class FastContactComponent {
   
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private route:Router) {
     translate.addLangs(['ge', 'en', 'ru']);
     translate.setDefaultLang('ge');
     const browerLang = translate.getBrowserLang();
   }
+
+  goToFacebook(): void {
+    window.location.href = 'https://www.facebook.com/tourismgeorgia111';
+}
+goToInstagram(): void {
+  window.location.href = 'https://www.instagram.com/admiraltravelgroup/?igshid=MjEwN2IyYWYwYw%3D%3D&fbclid=IwAR1Jsgyly6j0ps_uQ8ZPrvDOOMZJhg_B7iwAyOF0PZnC_ukJkMBWklgsLCo'
+
+}
 }
 
 
